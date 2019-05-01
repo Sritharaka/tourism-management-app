dbb4b2f9152fe16eed553954a3b2c0f5a1e65c18
Chandima Ranaweera <chan4lk@gmail.com>
2019-05-01 23:37:21 +0530

    register front end done

--- User/js/register.js
+++ User/js/register.js
@@ -1,11 +1,31 @@
 new Vue({
     el: '#main',
     data: {
-
+        name: '',
+        email: '',
+        password: '',
+        confirmPassword: '',
+        accept: false
     },
     methods: {
         submit: function(){
-            toastr.info('Submit')
+            if(this.password !== this.confirmPassword){
+                toastr.error('Passwords do not match');
+                return;
+            }
+            const data = {
+                name: this.name,
+                email: this.email,
+                password: this.password
+            }
+             Vue.http.post(APP_BASE_URL + '/api/users/register.php',data)
+                .then(response => {
+                    toastr.success('Regiser sucessfull');
+                    window.location.href = APP_BASE_URL + '/user/login.html';
+                })
+                .catch(err =>{
+                    toastr.error('Error Register');
+                })
         },
         cancel: function(){
             toastr.info('Cancel');
--- api/users/register.php
+++ api/users/register.php
@@ -9,26 +9,35 @@ header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers
 require_once('../../app/repository/user.php');
 
 $data = json_decode(file_get_contents("php://input"));
+try {
+    $repo = new UserRepository();
+    $result = $repo->insert($data->name, $data->email, $data->password);
+    if($result != null && $result != false){
+    
+        // set response code - 201 created
+        http_response_code(201);
 
-$repo = new UserRepository();
-$result = $repo->insert($data->name, $data->email, $data->password);
-if($result != null && $result != false){
- 
-    // set response code - 201 created
-    http_response_code(201);
+        // tell the user
+        echo json_encode($result);
+    }
 
-    // tell the user
-    echo json_encode($result);
+    // if unable to create the product, tell the user
+    else{
+
+        // set response code - 503 service unavailable
+        http_response_code(500);
+
+        // tell the user
+        echo json_encode(array('message'=>'user creation failed'));
+    }
+} catch (\Throwable $th) {
+    //throw $th;
+      // set response code - 503 service unavailable
+      http_response_code(500);
+
+      // tell the user
+      echo json_encode(array('message'=>'user creation failed'));
 }
 
-// if unable to create the product, tell the user
-else{
-
-    // set response code - 503 service unavailable
-    http_response_code(500);
-
-    // tell the user
-    echo json_encode(array('message'=>'user creation failed'));
-}
 
 ?>
